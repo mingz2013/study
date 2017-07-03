5e6000e5c39a742e64058b30161f297d9a0e3e97
mingz <305603665@qq.com>
2017-07-03 14:55:29 +0800

    rm opengl

--- c#/c#_form/Cookie_Qzone/Cookie_Qzone/JS/pgv_info.js
+++
@@ -1,6 +0,0 @@
-function func(a){
-	var f = (Math.round(Math.random() * 2147483647) * (new Date().getUTCMilliseconds())) % 10000000000;
-	var pgv_info= "ssid=s" + f;
-	return pgv_info
-	//return "123"
-}
